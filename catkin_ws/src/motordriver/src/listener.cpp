80dfd972a99b522ca28dae0626a69662477a1cdf
cheickis <cheick.ismo@gmail.com>
2022-11-23 12:27:44 -0500

    add driving functions

--- catkin_ws/src/motordriver/src/listener.cpp
+++ catkin_ws/src/motordriver/src/listener.cpp
@@ -8,17 +8,28 @@
 #include <unistd.h> //sleep
 
 
- struct roboclaw *rc;
+struct roboclaw *rc;
 int16_t voltage,  duty_cycle;
+uint8_t address=0x80; //address of roboclaw unit
 
-void initRoboclaw(){
-   rc=roboclaw_init("/dev/ttyACM0", 115200);
-}
 void checkBatteryStatus(){
  roboclaw_main_battery_voltage(rc, 0x80, &voltage);
  float voltage_float = (float)voltage/10.0f;
  ROS_INFO("Battery level= %f",voltage_float);
 }
+
+void initRoboclaw(){
+   ROS_INFO("Roboclaw Motor driver initilisation");
+   rc=roboclaw_init("/dev/ttyACM0", 115200);
+
+   if(rc==NULL){
+      ROS_FATAL("Cannot connect to the roboclaw driver!");
+    }else{
+       ROS_INFO("Connexion done!");
+   }
+  checkBatteryStatus();
+}
+
 
 int16_t getRobotSpeed(int16_t  sp){
     if( sp > 100 )
@@ -31,42 +42,125 @@ int16_t getRobotSpeed(int16_t  sp){
 }
 
 void closeRoboclaw(){
-   roboclaw_close(rc);
+
+   ROS_INFO("stopping the motors..\n");
+   roboclaw_duty_m1m2(rc, address, 0, 0);
+		
+   if(roboclaw_close(rc) != ROBOCLAW_OK)
+       ROS_ERROR("unable to shutdown roboclaw cleanly");
+   
 }
+
+int drive(int leftSp, int rightSp){
+     
+    int result = roboclaw_duty_m1m2(rc, address, leftSp, rightSp) ;
+   if( result!= ROBOCLAW_OK )
+   {
+     ROS_ERROR( "problem communicating with roboclaw, terminating\n");
+			return -1;			
+   } 
+  return ROBOCLAW_OK;
+}
+
+int left(int16_t sp){
+   int leftSp = getRobotSpeed( -sp);
+   int rightSp = getRobotSpeed(7);
+
+ 
+  int result = roboclaw_duty_m1m2(rc, address, -leftSp, -rightSp) ;
+   if( result!= ROBOCLAW_OK )
+   {
+     ROS_ERROR( "problem communicating with roboclaw, terminating\n");
+			return -1;			
+   } 
+  return ROBOCLAW_OK;
+
+}
+
+int right(int sp){
+  int rightSp = getRobotSpeed( sp);
+  int leftSp = getRobotSpeed(7);
+  int result = roboclaw_duty_m1m2(rc, address, leftSp, -rightSp) ;
+   if( result!= ROBOCLAW_OK )
+   {
+     ROS_ERROR( "problem communicating with roboclaw, terminating\n");
+			return -1;			
+   } 
+  return ROBOCLAW_OK;
+
+
+}
+
+
+int backward(int sp){
+  int mtrSp = getRobotSpeed( sp);
+  int result = roboclaw_duty_m1m2(rc, address, -mtrSp, mtrSp) ;
+   if( result!= ROBOCLAW_OK )
+   {
+     ROS_ERROR( "problem communicating with roboclaw, terminating\n");
+			return -1;			
+   } 
+  return ROBOCLAW_OK;
+
+}
+
+
+int forward(int sp){
+  int mtrSp = getRobotSpeed( sp);
+  int result = roboclaw_duty_m1m2(rc, address, mtrSp, -mtrSp) ;
+   if( result!= ROBOCLAW_OK )
+   {
+     ROS_ERROR( "problem communicating with roboclaw, terminating\n");
+			return -1;			
+   } 
+  return ROBOCLAW_OK;
+}
+
+
 void testRoboclaw()
 {
+      initRoboclaw();
       
-       
-        
-	
 }
 
 
+void testDriving(const std_msgs::Int16::ConstPtr& msg){
+   // duty_cycle = getRobotSpeed( msg->data)
+    //drive(duty_cycle, duty_cycle);
+    // forward(msg->data);
+    //backward(msg->data);
+   // left(msg->data);
+    right(msg->data);
 
-
-
+ }
 void chatterCallback(const std_msgs::Int16::ConstPtr& msg)
 {
 
-  testRoboclaw();
-  // ROS_INFO("I heard: [%d]", msg->data.c_str());
-   ROS_INFO("I heard: [%d]", msg->data);
-   //ROS_INFO("I heard:");
+   ROS_INFO("New speed Value: [%d]", msg->data);
+   testDriving(msg);
+ 
 }
 
+void timer_callback(const ros::TimerEvent &event){
 
+   ROS_INFO("In timer callback");
+}
 int main(int argc, char **argv)
 {
-   
+  
+   testRoboclaw();
   ros::init(argc, argv, "listener");
 
   ros::NodeHandle n;
 
+  
+  
   ros::Subscriber sub = n.subscribe("Chatter", 1000, chatterCallback);
 
   ros::spin();
 
-
+  closeRoboclaw();
+ 
   return 0;
 }
 
